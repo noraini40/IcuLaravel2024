e84e6d301a2f84c84833a9a179dc652bdf98ea41
noraini40 <noraini.mdnor1@gmail.com>
2024-10-15 10:41:57 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="widthdevice-witdth, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{config('app.name')}}</title>]
 </head>
 <body>
-    <h1> selamat datang</h1>
+    <h1> Hello, {{$name ?? "team" }}</h1>
 </body>
 </html>
